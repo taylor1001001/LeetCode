0c4b4dbd3e31836b41eca48515474ebe63a70dff
Taylor Ding <tayloryuyiding@gmail.com>
2021-11-11 12:15:23 -0500

    Create 455AssignCookies.java

---
+++ 455AssignCookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int result = 0;
+        
+        int j = 0;
+        for(int i=0; i<g.length; i++){
+            while(j<s.length){
+                if(s[j]>=g[i]){
+                    result++;
+                    j++;
+                    break;
+                }
+                j++;
+            }
+        }
+        
+        return result;
+    }
+}
