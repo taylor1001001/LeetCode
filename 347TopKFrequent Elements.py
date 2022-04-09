72d1dfa1996fde81e692ed2b486a3a7abae3f9a8
Taylor1001001 <tayloryuyiding@gmail.com>
2022-04-09 16:39:59 -0400

    Create 347TopKFrequent Elements.py

---
+++ 347TopKFrequent Elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k == len(nums):
+            return nums
+        
+        count = Counter(nums)
+        
+        return heapq.nlargest(k, count.keys(), count.get)
